fe2227bcce6ba326227672f2178f860bf42e52bd
MelissaDaCosta <mel-dacosta@hotmail.fr>
2019-11-02 18:46:52 +0100

    update travis again

---
+++ travis_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$FOLDER" = "lab6" ]; then
+    # download jdk-14
+    wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-linux.tar.gz
+    # extract
+    tar xvf jdk-14-loom-linux.tar.gz
+    # export new environment variable JAVA_HOME
+    export JAVA_HOME=$(pwd)/jdk-14-loom/
+fi
+
+# for all lab
+cd $FOLDER
+mvn package
