e93c31145c0985dcb604733e811d3f0f1d314c77
MelissaDaCosta <mel-dacosta@hotmail.fr>
2019-11-03 13:59:09 +0100

    update script for osx again

--- travis_script.sh
+++ travis_script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e # exit if return != 0
-if [ "$TARGET" = "lab6" ]; then
-    # download jdk-14
-    if[["$OSTYPE" == "linux-gnu"]]; then
-        wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-linux.tar.gz
-        tar xvf jdk-14-loom-linux.tar.gz
-    elif [["$OSTYPE" == "darwin17"]]; then
+if [ "$FOLDER" = "lab6" ]; then
+    if [ "$OSTYPE" = "darwin17" ]; then   # OS = osx
         wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-osx.tar.gz
         tar xvf jdk-14-loom-osx.tar.gz
+    else
+        # download jdk-14
+        wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-linux.tar.gz
+        # extract
+        tar xvf jdk-14-loom-linux.tar.gz
     fi
     # export new environment variable JAVA_HOME
     export JAVA_HOME=$(pwd)/jdk-14-loom/
 fi
 
 # for all lab
-cd $TARGET
+cd $FOLDER
 mvn package
