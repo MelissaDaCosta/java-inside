9aa78c561b361bfc0a2d23c23b9648accc1204d3
MelissaDaCosta <mel-dacosta@hotmail.fr>
2019-11-03 13:45:59 +0100

    update script for osx

--- travis_script.sh
+++ travis_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # exit if return != 0
 if [ "$FOLDER" = "lab6" ]; then
-    if["$OSTYPE" = "darwin17"]; then   # OS = osx
+    if[[ "$OSTYPE" = "darwin17" ]]; then   # OS = osx
         wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-osx.tar.gz
         tar xvf jdk-14-loom-osx.tar.gz
     else
